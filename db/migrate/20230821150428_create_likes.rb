09f2d85a710e7a51df0f0fd290ad6d51c1b58ad7
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-21 11:12:37 -0400

    migrated four tables success

--- db/migrate/20230821150428_create_likes.rb
+++ db/migrate/20230821150428_create_likes.rb
@@ -1,10 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :userId, null: false, foreign_key: true
-      t.references :postId, null: false, foreign_key: true
-      t.date :updated_at
-      t.date :created_at
+      t.references :userId, null: false, foreign_key: { to_table: :users }
+      t.references :postId, null: false, foreign_key: { to_table: :posts }
 
       t.timestamps
     end
--- db/migrate/20230821150440_create_comments.rb
+++ db/migrate/20230821150440_create_comments.rb
@@ -1,11 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :userId, null: false, foreign_key: true
-      t.references :postId, null: false, foreign_key: true
+      t.references :userId, null: false, foreign_key: { to_table: :users }
+      t.references :postId, null: false, foreign_key: { to_table: :posts }
       t.string :text
-      t.date :updated_at
-      t.date :created_at
 
       t.timestamps
     end
