c42975a94a001de019fd6387d2547cd354634d06
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-21 15:02:33 -0400

    change updates_likes_count method based on review

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-
+  after_save :update_likes_count
   def update_likes_count
-    update(likes_count: likes.count)
+    post.increment!(:likes_count)
   end
 end
