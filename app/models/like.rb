0c61cc41a316535488fa52399a4055714298405d
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-21 14:23:55 -0400

    Create custom methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+def update_likes_count
+  update(likes_count: likes.count)
+end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+after_create :increment_user_posts_count
+
+private
+
+def increment_user_posts_count
+  user.increment!(:posts_count)
+end
+def recent_comments
+  comments.order(created_at: :desc).limit(5)
+end
 end
