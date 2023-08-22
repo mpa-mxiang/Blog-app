bf43df34655aa451dd395f4982f73f7af45649a1
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-22 00:20:08 -0400

    fix comment tests
    delete comment text empty case since there is no related validation in comment.rb, add two invalidation cases and two method test cases

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,21 +7,35 @@ RSpec.describe Comment, type: :model do
     comment = Comment.new(user: user, post: post, text: 'Valid comment text')
     expect(comment).to be_valid
   end
-  it 'is invalid without text' do
-    user = User.create(name: 'John')
-    post = Post.create(title: 'Title', author: user)
-    comment = Comment.new(user: user, post: post, text: nil) # Provide nil text to trigger the validation error
+
+  it 'is invalid without a user' do
+    post = Post.create(title: 'Title', comments_counter: 0, likes_counter: 0)
+    comment = Comment.new(post: post, text: 'Invalid comment')
+    expect(comment).to_not be_valid
+  end
+
+  it 'is invalid without a post' do
+    user = User.create(name: 'John', posts_counter: 0)
+    comment = Comment.new(user: user, text: 'Invalid comment')
     expect(comment).to_not be_valid
   end
   
-  it 'increments the post comments_counter after creation' do
-    user = User.create(name: 'John')
-    post = Post.create(title: 'Title', author: user)
-  
+  it 'increments the comments_counter of the associated post after creating a comment' do
+    user = User.create(name: 'John', posts_counter: 0)
+    post = Post.create(title: 'Title', comments_counter: 0, likes_counter: 0, author: user)
+
     expect {
-      comment = Comment.create(user: user, post: post, text: 'Increment test')
-      post.reload # Reload the post to get the updated comments_counter
+      comment = post.comments.create(user: user, text: 'New Comment')
     }.to change { post.reload.comments_counter }.by(1)
+  end
+
+  it 'does not increment comments_counter if the comment creation fails' do
+    user = User.create(name: 'John', posts_counter: 0)
+    post = Post.create(title: 'Title', comments_counter: 0, likes_counter: 0, author: user)
+
+    expect {
+      comment = post.comments.create(user: nil, text: 'Invalid Comment')
+    }.not_to change { post.reload.comments_counter }
   end
   
 end
