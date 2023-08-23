9db5b4c6eeb9f09ecd932f367c17ecfa2c56b7cc
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-22 22:44:21 -0400

    autofix rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all # Fetch all posts from the database
-      end
-    def show
-        @post = Post.find(params[:id]) # Find a post by ID
-    end
-      
+  def index
+    @posts = Post.all # Fetch all posts from the database
+  end
+
+  def show
+    @post = Post.find(params[:id]) # Find a post by ID
+  end
 end
