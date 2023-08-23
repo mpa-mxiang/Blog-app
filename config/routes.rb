bc7e01ea9ea3bafb778bec96e2f01fb25f96d290
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-22 22:24:56 -0400

    define routes, create controllers and define actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+    # All users page
+    get '/users', to: 'users#index', as: 'users'
+
+    # Single user page
+    get '/users/:id', to: 'users#show', as: 'user'
+  
+    # All posts by a given user page
+    get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  
+    # Single post page
+    get '/posts/:id', to: 'posts#show', as: 'post'
+  
+    # ... Other routes for your application ...
+  
+    # Root route (optional)
+    root 'welcome#index'
 end
