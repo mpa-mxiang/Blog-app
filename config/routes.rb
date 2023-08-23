a0d522340ddd53a6380508a4e79c8a52b1922b59
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-23 15:52:00 -0400

    fix Request spec for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  # root "articles#index"
+    # All users page
+    get '/users', to: 'users#index', as: 'users'
+
+    # Single user page
+    get '/users/:id', to: 'users#show', as: 'user'
   
-    # Root route (optional)
-    root 'welcome#index'
+    # All posts by a given user page
+    get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  
+    # Single post page
+    get '/posts/:id', to: 'posts#show', as: 'post'
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,21 +1,47 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "UsersController", type: :request do
   describe "GET /users" do
-    it "renders the index template" do
+    it "returns a successful response" do
       get users_path
       expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get users_path
       expect(response).to render_template(:index)
+    end
+
+    it "displays user information" do
+      # Create some sample users manually
+      user1 = User.create(name: "User 1")
+      user2 = User.create(name: "User 2")
+
+      get users_path
       expect(response.body).to include("All Users")
+      expect(response.body).to include(user1.name)
+      expect(response.body).to include(user2.name)
     end
   end
 
   describe "GET /users/:id" do
-    it "renders the show template" do
-      user = create(:user) # Assuming you have a Factory Bot factory for User
+    it "returns a successful response" do
+      user = User.create(name: "Test User")
       get user_path(user)
       expect(response).to have_http_status(200)
+    end
+
+    it "renders the show template" do
+      user = User.create(name: "Test User")
+      get user_path(user)
       expect(response).to render_template(:show)
+    end
+
+    it "displays user information" do
+      user = User.create(name: "Test User")
+
+      get user_path(user)
+      expect(response.body).to include("User Details")
       expect(response.body).to include(user.name)
     end
   end
