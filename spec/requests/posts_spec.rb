9b33f9f67254965049956df19dc89be33fc67abf
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-23 22:23:25 -0400

    fix Respect spec for posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,41 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /posts' do
-    it 'returns a successful response' do
-      get posts_url
+RSpec.describe 'PostsController', type: :request do
+  let(:user) { User.create(name: 'Heather', photo: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80', bio: 'young beauty woman.') }
+  let(:post) { Post.create(author: user, title: 'sample title', text: 'sample post') }
+
+  describe 'GET /users/:id/posts' do
+    it 'renders the index template and includes placeholder text' do
+      get user_posts_path(user)
       expect(response).to have_http_status(200)
-    end
-
-    it 'renders the index template' do
-      get posts_url
       expect(response).to render_template(:index)
-    end
-
-    it 'includes correct placeholder text in the response body' do
-      get posts_url
-      expect(response.body).to include('Sample Title')
-      expect(response.body).to include('Sample Text')
     end
   end
 
-  describe 'GET /posts/:id' do
-    let(:post) { Post.create(title: 'Sample Title', text: 'Sample Text') }
-
-    it 'returns a successful response' do
-      get post_url(post)
+  describe 'GET /users/:id/posts/:id' do
+    it 'renders the show template and includes placeholder text' do
+      get user_post_path(user, post)
       expect(response).to have_http_status(200)
-    end
-
-    it 'renders the show template' do
-      get post_url(post)
       expect(response).to render_template(:show)
-    end
-
-    it 'includes correct placeholder text in the response body' do
-      get post_url(post)
-      expect(response.body).to include('Sample Title')
-      expect(response.body).to include('Sample Text')
+      expect(response.body).to include('<h5 class="card-title">Post X by username</h5>')
     end
   end
 end
