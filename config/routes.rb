199a3939642d44fa8ab3ee15ebbbb4676cad2801
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-30 16:14:50 -0400

    allow user to like

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
+      post 'like', on: :member
       resources :comments, only: [:create]
     end
   end
