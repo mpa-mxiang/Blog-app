64e890f39ffacc97df6ff0a4e80f0a196af1b59f
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-30 22:59:34 -0400

    add new post and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = current_user
     @posts = @user.recent_posts.includes(:comments)
   end
 
@@ -9,4 +9,25 @@ class PostsController < ApplicationController
     @user = @post.author
     @comment = Comment.new
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to user_posts_path(current_user), notice: 'Post was successfully created.'
+    else
+      @posts = current_user.recent_posts.includes(:comments) # Add this line to update @posts
+      render 'new'
+    end
+  end
+  
+  
+  def new
+    @post = Post.new
+  end
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
