5d052fdc943016d64ced444ad39eb6761ed25915
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-30 22:09:40 -0400

    fix comment counter add by 2 issue

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(text: comment_params[:text]) # Use the correct parameter name
+    @comment = @post.comments.build(text: comment_params[:text])
     @comment.user = current_user
   
     if @comment.save
-      @post.increment!(:comments_counter)
-      redirect_to user_post_path(@post.author, @post), notice: 'Comment was successfully created.'
+      redirect_to user_post_path(@post.author, @post)
     else
       render 'posts/show'
     end
